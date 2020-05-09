78c734105ba0e4f384beb5783512d9502df24ab4
sawarabhattarai5 <60766679+sawarabhattarai5@users.noreply.github.com>
2020-05-08 23:32:01 -0700

    Add files via upload

---
+++ classopedia 2/classopedia/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  classopedia
+//
+//  Created by Sawara Bhattarai on 4/6/20.
+//  Copyright © 2020 Sawara Bhattarai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
